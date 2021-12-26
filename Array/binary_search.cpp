8bc02ba59e7460681c5732a404507cf7969842ed
vignesh1398-vz <vigneswaran.s@viewzenlabs.com>
2021-12-26 20:31:47 +0530

    Recursive binary_search in an array

--- Array/binary_search.cpp
+++ Array/binary_search.cpp
@@ -15,12 +15,30 @@ int binary_search(int * arr, int n ,int key) {
     return -1;
 }
 
+int recursive_binary_search(int *arr, int key, int start, int end) {
+    int mid = (start + end)/2;
+    if(start > end) 
+        return -1;
+    else if(arr[mid] == key) 
+        return mid;
+    else if(arr[mid] > key) 
+        return recursive_binary_search(arr, key, start, mid - 1);
+    else
+        return recursive_binary_search(arr, key, mid + 1, end);
+}
+
 int main(int argc, char** argv) {
     int arr[] = {1, 2, 3, 4, 5, 6, 7};
     cout << binary_search(arr, 7, 7) << "\n";
     cout << binary_search(arr, 7, 1) << "\n";
     cout << binary_search(arr, 7, -9) << "\n";
     cout << binary_search(arr, 7, 5) << "\n";
-    cout << binary_search(arr, 4, 5) << "\n";
-   return 0;
+    cout << binary_search(arr, 7, 5) << "\n";
+
+    cout << recursive_binary_search(arr, 7, 0, 6) << "\n";
+    cout << recursive_binary_search(arr, 1, 0, 6) << "\n";
+    cout << recursive_binary_search(arr, -9, 0, 6) << "\n";
+    cout << recursive_binary_search(arr, 5, 0, 6) << "\n";
+    cout << recursive_binary_search(arr, 5, 0, 6) << "\n";
+    return 0;
 }
