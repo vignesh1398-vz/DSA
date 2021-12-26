347fb73a56c17d2c2ed79c8ba37c3a058ca701f0
vignesh1398-vz <vigneswaran.s@viewzenlabs.com>
2021-12-26 22:11:08 +0530

    Maximum subarray sum (Kadane's Algorithm)

--- Array/subarray_sum.cpp
+++ Array/subarray_sum.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/* O(N^3) time complexity (Brute force approach) */
 int brute_force(int *arr, int n) {
     int largestSum = 0, subArraySum = 0;
     for(int i = 0; i < n; i++) {
@@ -15,8 +16,21 @@ int brute_force(int *arr, int n) {
     return largestSum;
 }
 
+/* Kadane's Algorithm O(n) approach */
+int kadane_algorithm(int *arr, int n) {
+    int current_sum = 0, max_sum = 0;
+    for(int i = 0; i < n; i++) {
+        current_sum += arr[i];
+        if(current_sum < 0) 
+            current_sum = 0;
+        max_sum = max(max_sum, current_sum);
+    }
+    return max_sum;
+}
+
 int main(int argc, char **argv) {
     int arr[] = {-2, 3, 4, -1, 5, -12, 6, 1, 3};
     cout << brute_force(arr, 9) << '\n';
+    cout << kadane_algorithm(arr, 9) << '\n';
     return 0;
 }
