2de06b6dd1d7331cf22aaaf75370b4314fbe58dc
vignesh1398-vz <vigneswaran.s@viewzenlabs.com>
2022-01-01 16:38:34 +0530

    Get ith bit

---
+++ Bitwise Operation/common_operations.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int getIthBit(int number, int bit_position) {
+    int mask = (1 << bit_position);
+    return (number & mask) > 0 ? 1 : 0;
+}
+
+int main(int argc, char **argv) {
+    int number, bit_position;
+    cout << "Enter number: ";
+    cin >> number;
+    cout << "\nEnter bit position: ";
+    cin >> bit_position;
+
+    cout << "\n" << getIthBit(number, bit_position);
+
+    return 0;
+}
