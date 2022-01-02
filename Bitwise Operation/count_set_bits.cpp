a7d3d5171a5a207e9d0392d4b808e3c99958f10b
vignesh1398-vz <vigneswaran.s@viewzenlabs.com>
2022-01-02 20:32:59 +0530

    Count set bit hack(unorthodox)

--- Bitwise Operation/count_set_bits.cpp
+++ Bitwise Operation/count_set_bits.cpp
@@ -11,12 +11,22 @@ int set_bits_count(int n) {
     return count;
 }
 
+int set_bits_count_hack(int n) {
+    int count = 0;
+    while(n > 0) {
+        n = n & (n-1);
+        count++;
+    }
+    return count;
+}
+
 int main(int argc, char **argv) {
     int number;
     cout << "Enter number: ";
     cin >> number;
 
-    cout << '\n' << set_bits_count(number);
+    cout << set_bits_count(number);
+    cout << '\n' << set_bits_count_hack(number);
 
     return 0;
 }
